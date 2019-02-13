30dcfe7fc2af909dba949b6a4a98efd73b98c3e5
Sander Lindberg <Sander.b.lindberg@gmail.com>
2019-02-13 12:17:09 +0100

    Started on restaurant model

--- trippinTacos/reservations/models.py
+++ trippinTacos/reservations/models.py
@@ -4,11 +4,9 @@ from django.utils import timezone
 # Create your models here.
 
 
-class Reservation(models.Model):
-    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
-    date = models.DateField()
-    time = models.TimeField()
-    created_date = models.DateTimeField(default=timezone.now) #added this field for later use when adding statistics
+
+class Restaurant(models.Model):
+    
 
 
 class Table(models.Model):
@@ -17,3 +15,9 @@ class Table(models.Model):
     reservation = models.ForeignKey(Reservation, models.SET_NULL, blank=True, null=True)
 
 
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    start_date_time = models.DateTimeField()
+    end_date_time = models.DateTimeField()
+    created_date = models.DateTimeField(default=timezone.now) # added this field for later use when adding statistics
+    table = models.ForeignKey(Table, on_delete=pass)
