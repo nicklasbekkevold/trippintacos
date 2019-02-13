d0db4fe6ac9a39c7192acd6da06af4d42c8f6dd1
ialml <aimeo@stud.ntnu.no>
2019-02-13 14:01:38 +0100

    Reservasjonsklasse, som man kan booke fra. Har laget logikk for make_reservation(), skal skrive logikk for get_next_available_table

--- trippinTacos/reservations/Reservation.py
+++ trippinTacos/reservations/Reservation.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
 from .models import Reservation, Table, Guest
+from datetime import datetime
+
 
 def get_next_available_table(restaurant, reservation_date_time, number_of_people, minutes_slot = 120):
 
@@ -11,10 +13,11 @@ def make_reservation(restaurant, guest, reservation_date_time,number_of_people, 
 
     if table:
         delta = timedelta(seconds=60*minutes_slot)
-        reservation = Reservation(guest = guest, number_of_people = number_of_people, )
-
-
-
+        reservation = Reservation(guest = guest, number_of_people = number_of_people, start_date_time = reservation_date_time, end_date_time = reservation_date_time + delta, createdDate = datetime.now(), table = table)
+        reservation.save()
+        return {'reservation': reservation.id, 'table': table.id}
+    else:
+        return None
 
 
 
