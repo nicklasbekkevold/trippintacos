5c3ce99c136b8384f3621be4e98f157c872fc220
Sander Lindberg <Sander.b.lindberg@gmail.com>
2019-02-13 09:57:54 +0100

    Added models for reservation, table and guest, updated gitignore

--- trippinTacos/reservations/models.py
+++ trippinTacos/reservations/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from guest.models import Guest
 # Create your models here.
+
+
+class Reservation(models.Model):
+    guest = models.ForeignKey(Guest, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+
+
+class Table(models.Model):
+    number_of_seats = models.IntegerField()
+    is_occupied = models.BooleanField()
+    reservation = models.ForeignKey(Reservation, models.SET_NULL, blank=True, null=True)
