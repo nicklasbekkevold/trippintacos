eead1f22b8cf10b84fef42700cb42efe5a609637
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2019-02-19 19:41:29 +0100

    feat: rename static to bootstrap. update static settings

--- trippinTacos/settings.py
+++ trippinTacos/settings.py
@@ -93,8 +93,12 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-
 }
+
+try: 
+    from .local_settings import *
+except:
+    pass
 
 
 # Password validation
@@ -135,12 +139,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'bootstrap'),
+   ]
 
-try: 
-    from local_settings import *
-except ImportError:
-    pass
 
 
 db_from_env = dj_database_url.config(conn_max_age=500)
