bd0c731ca5536cf027764d2e4b04f2591f8be524
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2019-02-19 18:33:32 +0100

    add Procfile

---
+++ Procfile
@@ -0,0 +1,3 @@
+cd trippinTacos/
+python manage.py migrate
+python manage.py runserver
--- trippinTacos/trippinTacos/settings.py
+++ trippinTacos/trippinTacos/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lry^$zfx8ca0y^x%deyi=6t7$yhhly8cct9h^*3t38%$zovxry'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
