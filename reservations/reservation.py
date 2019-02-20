94a12ce7f56943b8d43c3e3b4d6c2dcefa36d498
Sander Lindberg <Sander.b.lindberg@gmail.com>
2019-02-20 15:49:32 +0100

    Reservations

--- reservations/reservation.py
+++ reservations/reservation.py
@@ -13,56 +13,66 @@ def get_next_available_table(restaurant, reservation_date_time, number_of_people
 
     # ekskluder allerede bookede bord som inneholder den initielle booking_date_time
 
-    tables_booked = Reservation.objects.filter(table_restaurant=restaurant,
-                                               reservation_date_time_start__lt=lower_bound_time,
-                                               reservation_date_time_end__gt=lower_bound_time).values('table')
+    tables_booked = Reservation.objects.filter(table__restaurant=restaurant,
+                                               start_date_time__lt=lower_bound_time,
+                                               end_date_time__gt=lower_bound_time).values('table')
     tables_booked_ids_temp = [x['table'] for x in tables_booked]
     tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
 
     # ekskluder allerede bookede bord som har den etterspurte sluttiden
 
-    tables_booked = Reservation.objects.filter(reservation_date_time_start_lt=upper_bound_time,
-                                               reservation_date_time_end__gt=upper_bound_time).values('table')
+    tables_booked = Reservation.objects.filter(start_date_time__lt=upper_bound_time,
+                                               end_date_time__gt=upper_bound_time).values('table')
     tables_booked_ids_temp = [x['table'] for x in tables_booked]
     tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
 
     # ekskluderer bookede bord som er inni den aktuelle tidsperioden. er dette jalla?
 
-    tables_booked = Reservation.objects.filter(reservation_date_time_start__gt=lower_bound_time,
-                                               reservation_date_time_end__lt=upper_bound_time).values('table')
+    tables_booked = Reservation.objects.filter(start_date_time__gt=lower_bound_time,
+                                               end_date_time__lt=upper_bound_time).values('table')
     tables_booked_ids_temp = [x['table'] for x in tables_booked]
     tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
 
     # ekskluderer bord som inkluderer den etterspurte booking-luken
 
-    tables_booked = Reservation.objects.filter(booking_date_time_start__lt=lower_bound_time,
-                                               booking_date_time_end__gt=upper_bound_time).values('table')
+    tables_booked = Reservation.objects.filter(start_date_time__lt=lower_bound_time,
+                                               end_date_time__gt=upper_bound_time).values('table')
     tables_booked_ids_temp = [x['table'] for x in tables_booked]
     tables_booked_ids = tables_booked_ids + tables_booked_ids_temp
 
     # lager liste med alle bord av nødvendig størrelse, tilgjengelige i restauranten
     # ekskluderer den forrige listen av utilgjengelige bord. Listen er rangert fra minst til størst tilgjengelig kapasitet,
     # og det ledige bordet med minst kapasitet returneres.
+    print("NUMBER OF PEOPLE:", number_of_people)
+    differance = reservation_date_time
+    (minutes_slot / float(60))
+    temp_time_hardcode = (minutes_slot / float(60))
 
-    tables = Table.objects.filter(restaurant=restaurant, restaurant__opening_time_lte=reservation_date_time.hour,
-                                  restaurant__closing_time__gte=reservation_date_time.hour + (minutes_slot / float(60)),
-                                  size__gte=number_of_people).exclude(id__in=tables_booked_ids).order_by('size')
+    tables = Table.objects.filter(
+        restaurant=restaurant,
+        restaurant__opening_time__lte=str(reservation_date_time.hour) + ":" + str(reservation_date_time.minute),
+        restaurant__closing_time__gte=str((reservation_date_time + timedelta(hours=temp_time_hardcode)).hour) + ":" +
+        str((reservation_date_time + timedelta(hours=temp_time_hardcode)).minute),
+        number_of_seats__gte=number_of_people)# .exclude(id__in=tables_booked_ids).order_by('number_of_seats')
 
+    print("opening time", str(reservation_date_time.hour) + ":00")
+    print("closing time", str((reservation_date_time + timedelta(hours=temp_time_hardcode)).hour) + ":00")
+    print("TABLES: ", tables)
     if tables.count() == 0:
         return None
     else:
         return tables[0]
 
 
-def make_reservation(restaurant, guest, reservation_date_time, number_of_people, minutes_slot=120, ):
+def make_reservation(restaurant, guest, reservation_date_time, number_of_people, minutes_slot=120):
     # funksjon som bruker get_next_available_table for å reservere et ledig bord på et ledig tidspunkt
-
-    table = get_next_available_table(restaurant, reservation_date_time, minutes_slot)
+    # print("NUMBER OF PEOPLE:", number_of_people)
+    table = get_next_available_table(restaurant, reservation_date_time, number_of_people, minutes_slot)
 
     if table:
         delta = timedelta(seconds=60 * minutes_slot)
         reservation = Reservation(guest=guest, number_of_people=number_of_people, start_date_time=reservation_date_time,
-                                  end_date_time=reservation_date_time + delta, createdDate=datetime.now(), table=table)
+                                  end_date_time=reservation_date_time + delta, created_date=datetime.now(), table=table)
         reservation.save()
         return {'reservation': reservation.id, 'table': table.id}
     else:
--- reservations/views.py
+++ reservations/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .forms import ReservationForm
 from django.shortcuts import redirect
 from guest.models import *
+from reservations.models import *
+from reservations.reservation import make_reservation
 
 
 # Create your views here.
@@ -12,17 +14,26 @@ def booking(request):
         form = ReservationForm(request.POST)
 
         if form.is_valid():
-            email = form.cleaned_data['email']
+            email = form.cleaned_data['email'].lower()
             email_liste = []
             for each in Guest.objects.all():
-                email_liste.append(each)
+                email_liste.append(each.email.lower())
 
+            print("EMAIL LISTE:", email_liste, "EMAIL:", email)
             if email not in email_liste:
-                Guest.objects.create(email, form.cleaned_data['reminder'])
+                guest = Guest(email=email, reminder=form.cleaned_data['reminder'])
+                # guest = Guest.objects.create(email=email, reminder=form.cleaned_data['reminder'])
+                guest.save()
+                print("GUEST: ", guest)
+            else:
+                guest = Guest.objects.all().get(email=email)
+                print("GUEST: ", guest)
+            # form.guest = guest
+            print(make_reservation(Restaurant.objects.first(), guest, form.cleaned_data['start_date_time'], form.cleaned_data['number_of_people']))
 
-            post = form.save()
-            post.save()
-            return redirect(request, 'base.html')
+            # post = form.save()
+            # post.save()
+            return render(request, 'success.html')
 
     else:
         form = ReservationForm()
