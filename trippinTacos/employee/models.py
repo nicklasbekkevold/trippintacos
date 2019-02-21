598ca9c34950881077aced4de76e49dafc16e560
JakobDRoti <jakobdr@stud.ntnu.no>
2019-02-21 13:55:57 +0100

    employee models

--- trippinTacos/employee/models.py
+++ trippinTacos/employee/models.py
@@ -3,7 +3,7 @@ from guest.models import Guest
 # Create your models here.
 
 
-class Employee(models.  Model):
+class Employee(models.Model):
     first_name = models.CharField(max_length=25)
     last_name = models.CharField(max_length=25)
     
