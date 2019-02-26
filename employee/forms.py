dbe1987b164bc21caaf78520bb0d819d53e68653
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2019-02-26 13:55:34 +0100

    Added table-solution to reservation visualization

--- employee/forms.py
+++ employee/forms.py
@@ -3,5 +3,5 @@ from bootstrap_datepicker_plus import *
 
 class DateForm(forms.Form):
     current_date = forms.DateField(
-        widget=DatePickerInput(format='%A %m-%Y') #TODO fix formatting here by: format=' ... '
+        widget=DatePickerInput(format='%d-%m-%Y') #TODO fix formatting here by: format=' ... '
     )
