dc86764cfe1609342c9cd00fccbda881ec98f22f
Nicklas Bekkevold <nicklasbekkevold@gmail.com>
2019-03-28 12:39:13 +0100

    Change media/static url

--- trippinTacos/settings.py
+++ trippinTacos/settings.py
@@ -143,8 +143,8 @@ TIME_ZONE = 'Europe/Oslo'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
